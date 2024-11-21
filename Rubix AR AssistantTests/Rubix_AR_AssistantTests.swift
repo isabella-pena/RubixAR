5be387295791a2b0b5cbd8f51abd5679e4bae494
isabella-pena <isabella.pena@berkeley.edu>
2024-11-21 09:44:46 -0800

    Initial commit

---
+++ Rubix AR AssistantTests/Rubix_AR_AssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  Rubix_AR_AssistantTests.swift
+//  Rubix AR AssistantTests
+//
+//  Created by Isabella Pena on 11/21/24.
+//
+
+import Testing
+@testable import Rubix_AR_Assistant
+
+struct Rubix_AR_AssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Rubix AR AssistantUITests/Rubix_AR_AssistantUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Rubix_AR_AssistantUITestsLaunchTests.swift
+//  Rubix AR AssistantUITests
+//
+//  Created by Isabella Pena on 11/21/24.
+//
+
+import XCTest
+
+final class Rubix_AR_AssistantUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
